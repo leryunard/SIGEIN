329f552efc0ce941117f9a07978f61cf1279cb7b
Rodrigo-java <71677652+Rodrigo-java@users.noreply.github.com>
2023-06-07 21:32:54 -0600

    mejoras
    Mensaje centrado
    imagen de ico.
    Eliminación de copyringh

--- layout/parte1.php
+++ layout/parte1.php
@@ -8,7 +8,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Sistema de Inventario</title>
-
+  <link rel="icon"  href="../imagen/logo.ico">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
@@ -37,7 +37,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link"><p><strong>SISTEMA DE INVENTARIO LUBRICANTES Y ACCESORIOS ESPAÑA</strong></a></p>
+        <a href="#" class="nav-link"><p><A>SISTEMA DE INVENTARIO DE LUBRICANTES Y ACCESORIOS ESPAÑA</a></a></p>
       </li>
     </ul>
 
@@ -79,7 +79,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
     <a href="<?php echo $URL;?>/index.php" class="brand-link">
       <img src="<?php echo $URL;?>../imagen/logo.jpg" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light"><strong> Estas en el Inventario</strong></span>
+      <span class="brand-text font-weight-light"><strong> Inventario</strong></span>
     </a>
 
     <!-- Sidebar -->
